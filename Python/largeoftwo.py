1d9c6fff06f2e719dd0e961724ecaf2c5128c988
Abdullah Ahmed Khan <ahmedamk42@gmail.com>
2026-07-31 17:12:20 +0530

    commit

---
+++ Python/largeoftwo.py
@@ -0,0 +1,8 @@
+#Largest of two numbers
+num1 = float(input("Enter number 1 = "))
+num2 = float(input("Enter number 2 = "))
+
+if num1>num2:
+    print(f"{num1} is the largest")
+else:
+    print(f"{num2} is the largest")
---
+++ Python/leapyear..py
@@ -0,0 +1,6 @@
+year = int(input("Enter Year: "))
+
+if (year%4==0 and year%100 != 0 )or (year%400==0):
+    print("The year is a leap year")
+else:
+    print("This is not a leap year")
