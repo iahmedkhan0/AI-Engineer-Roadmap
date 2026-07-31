c978a4e51a085f3c53aa9f807a1a546d021a6a46
Abdullah Ahmed Khan <ahmedamk42@gmail.com>
2026-07-31 16:35:05 +0530

    commit

---
+++ Python/10.py
@@ -0,0 +1,7 @@
+#Area of rectangle
+length = float(input("Enter the length of Rectangle = "))
+breadth = float(input("Enter the breadth of Rectangle = "))
+
+area = length*breadth
+
+print("The are of rectangle is = ",area)
